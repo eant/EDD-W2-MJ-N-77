7ce06e70eaf736f7a41788c99b5c3e53ed372eb5
smessina <silvio.messina@eant.tech>
2020-05-14 20:27:54 -0300

    Simplificando JS con map() y Arrow Function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hola desde la terminal")
+const express = require("express")
+
+const server = express()
+
+const docs = express.static("docs")
+
+server.use( docs )
+
+server.get("/ofertas.html", function(request, response){
+    response.end("Acá van a mostrarse las ofertas de MercadoTECH")
+})
+
+server.post("/enviar", function(req, res){
+    res.end("Aca debo ver el enviar los datos del formulario")
+})
+
+server.listen(2000)
---
+++ docs/tiposDeFuncionesES6.js
@@ -0,0 +1,19 @@
+//ES5 - Sugar Syntax
+function Mostrar(){
+
+}
+//ES5 - Real Syntax
+var Mostrar = function(){
+
+}
+////////////////////////////////
+//ES6 - Arrow Function
+const Mostrar = () => {
+
+}
+const Buscar = precio => {
+
+}
+const Comparar = (producto1, producto2) => {
+
+}
